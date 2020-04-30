cf6ceac8ebd657d64a0aa2384243f53af60f1c54
Jegan K <jegancbk@gmail.com>
2020-04-30 17:39:36 -0500

    bindings demo 3

---
+++ BindingDemo3/app/src/main/java/com/jegan/bindingdemo3/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.jegan.bindingdemo3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.jegan.bindingdemo3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val student =getStudent()
+        binding.nameText.text = student.name
+        binding.emailText.text = student.email
+    }
+
+    private fun getStudent():Student{
+        return Student(1,"Alex","alex@gmail.com")
+    }
+}
