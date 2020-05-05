1d97a5c6c883c49384d4f3d49f6289a42db3ae46
Jegan K <jegancbk@gmail.com>
2020-05-04 19:53:55 -0500

    view model factory demo with Live data

--- viewmodeldemo2/app/src/main/java/com/jegan/viewmodeldemo2/MainActivity.kt
+++ viewmodeldemo2/app/src/main/java/com/jegan/viewmodeldemo2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jegan.viewmodeldemo2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.jegan.viewmodeldemo2.databinding.ActivityMainBinding
 
@@ -18,10 +19,11 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
 
-        binding.textView.text = viewModel.getTotal().toString()
+        viewModel.totalData.observe(this, Observer {
+            binding.textView.text = it.toString()
+        })
         binding.button.setOnClickListener {
             viewModel.setTotal(binding.editText.text.toString().toInt())
-            binding.textView.text = viewModel.getTotal().toString()
         }
 
 
--- viewmodeldemo2/app/src/main/java/com/jegan/viewmodeldemo2/MainActivityViewModel.kt
+++ viewmodeldemo2/app/src/main/java/com/jegan/viewmodeldemo2/MainActivityViewModel.kt
@@ -1,20 +1,22 @@
 package com.jegan.viewmodeldemo2
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int): ViewModel() {
 
-    private var totalSum = 0
+    private var totalSum = MutableLiveData<Int>()
+
+    val totalData: LiveData<Int>
+    get() = totalSum
 
     init {
-        totalSum = startingTotal
+        totalSum.value = startingTotal
     }
 
-    fun getTotal(): Int {
-        return totalSum
-    }
 
     fun setTotal(value: Int) {
-        totalSum += value
+        totalSum.value = totalSum.value ?.plus(value)
     }
 }
